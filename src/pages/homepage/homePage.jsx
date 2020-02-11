02f7944ef01037b02939cb625b605c543a155ca2
SidWorks <51191127+SidWorks@users.noreply.github.com>
2020-02-11 11:33:44 +0530

    adding files with styles

---
+++ src/pages/homepage/homePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homePage.scss";
+
+import Directory from "../../Components/directory/directory.component";
+
+const HomePage = () => {
+  return <Directory />;
+};
+export default HomePage;
