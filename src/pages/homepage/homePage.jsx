a05c40451ad103e38db40ca2a505979c888f4660
SidWorks <51191127+SidWorks@users.noreply.github.com>
2020-04-01 13:06:54 +0530

    added shop item with styles

--- src/pages/homepage/homePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./homePage.scss";
-
-import Directory from "../../Components/directory/directory.component";
-
-const HomePage = () => {
-  return <Directory />;
-};
-export default HomePage;
